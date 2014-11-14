a5ce382163f91efb7612d2b7616e60ccd53fbb4d
Georg Hofmann <georg.w.hofmann@gmail.com>
2014-11-14 17:27:55 +0000

    Visualization now works.

--- StandAloneScripts/AWS-Visualize-EGH.R
+++ StandAloneScripts/AWS-Visualize-EGH.R
@@ -1,4 +1,4 @@
-require(maps)
+require(maps) # install.packages("maps")
 require(gwh.tools)
 require(Matrix)
 
@@ -10,7 +10,7 @@ s3.input.folder <- "EGHoriginal_PCADD"
 working.directory <- "~/data/"
 setwd(working.directory)
 
-s3.input.path <- paste(s3.data.path, s3.input.folder, "_MetaOld/", sep="")
+s3.input.path <- paste(s3.data.path, s3.input.folder, "_Meta/", sep="")
 s3.output.path <- paste(s3.data.path, s3.input.folder, "_Vis/", sep="")
 
 #s3.put(file = "~/Config.R", s3.path = paste(s3.output.path, "Config.R", sep=""))
@@ -20,10 +20,12 @@ s3.output.path <- paste(s3.data.path, s3.input.folder, "_Vis/", sep="")
 result.list <- s3.readRDS(s3.path = paste(s3.input.path, "ResultBin.RDS", sep=""))
 grid.matrix <- result.list$grid.matrix
 grid.meta <- s3.read.csv(s3.path = paste(s3.input.path, "GridMeta.csv", sep=""))
-grid.filter <- grid.meta$grid.id!=0
+grid.filter <- grid.meta$grid.id>0
 grid.meta <- grid.meta[grid.filter,] 
 grid.matrix <- grid.matrix[grid.filter,] 
 grid.id <- grid.meta$grid.id
+
+hazard.meta <- s3.read.csv(s3.path = paste(s3.input.path, "HazardMeta.csv", sep=""))
 
 exc.rate.vector <- c(.0001, .0002, .0005, .001, .002, .005, .01, .02, .05, .1, .2, .5, 1)
 
@@ -34,26 +36,25 @@ map.er.vector <- -log(1 - 1 / map.rp.vec)
 
 
 bucket.table <- s3.read.csv(s3.path = paste(s3.input.path, "BucketMeta.csv", sep=""))
-hazard.id <- unique(bucket.table$haz.id)
 
 num.records <- nrow(grid.matrix)
 
 
-for(haz.i in 1:length(hazard.id)){
-  col.filter <- which(threshold.table$haz.name==hazard.name[haz.i])
+for(haz.i in 1:nrow(hazard.meta)){
+  col.filter <- which(bucket.table$haz.id==hazard.meta$haz.id[haz.i])
   exc.rate.mat <- as.matrix(grid.matrix[,col.filter])
   for(i in (ncol(exc.rate.mat) - 1L):1L){
     exc.rate.mat[,i] <- exc.rate.mat[,i] + exc.rate.mat[,i + 1]
-  }
-  
-  #diff(colSums(exc.rate.mat))
-  
-  haz.threshold <- threshold.table$haz.val[col.filter]
+  }  
+  haz.threshold <- bucket.table$bucket.from[col.filter]
+  haz.threshold[1] <- 0
   haz.mat <- matrix(data=0, nrow=num.records, ncol=length(exc.rate.vector))
   rev.col.filter <- col.filter[order(col.filter, decreasing = TRUE)]
-  log.start.seconds <- BeginTimedLog("Interpolating data for ", hazard.name[haz.i])
+  log.start.seconds <- BeginTimedLog("Interpolating data for ", hazard.meta$haz.name[haz.i])
   for(i in 1: num.records){
     x <- c(0, as.vector(exc.rate.mat[i, ncol(exc.rate.mat):1]))
+    # Note that we assume that rate has 
+    # decayed twice the max hazard value:
     y <- c(max(haz.threshold) * 2, haz.threshold[ncol(exc.rate.mat):1])
     haz.mat[i,] <- 
       approx(yright = 0, ties = "ordered",
@@ -66,23 +67,26 @@ for(haz.i in 1:length(hazard.id)){
   
   colnames(haz.mat) <- exc.rate.vector
   rownames(haz.mat) <- grid.id
-  write.csv(haz.mat, paste(hazard.name[haz.i], "_ER.csv", sep=""))
+  s3.write.csv(object = haz.mat, 
+               s3.path = paste(s3.output.path, hazard.meta$haz.name[haz.i], "_ER.csv", sep=""))
   EndTimedLog(log.start.seconds)
-  max(haz.mat[,1])
-  color.threshold <- haz.threshold
+
+  color.threshold <- haz.threshold[-1]
   round.col.thr <- round(color.threshold, 2)
   color.pal <- colorRampPalette(c("gray", "yellow", "red"))(length(color.threshold) + 1L)
   #rainbow(n=length(color.threshold) + 1L)
   
-  
+  if(!file.exists("RP-Maps")){
+    dir.create("RP-Maps")
+  }
   for(er.i in 1:length(map.rp.vec)){
-    filename <- paste("RP-Maps/", hazard.name[haz.i], "_", 
-                      sprintf("%05.0f", map.rp.vec[er.i]), ".png", sep="") 
+    filename <- paste("RP-Maps/", hazard.meta$haz.name[haz.i], "_", 
+                      sprintf("%05.0f", map.rp.vec[er.i]), ".png", sep="")     
     png(filename, width = 2400, height = 1600, pointsize=48)
     grid.index <- match(grid.id, grid.meta$grid.id)
     lat <- grid.meta$lat[grid.index]
     lon <- grid.meta$lon[grid.index]
-    filter <- !is.â¦na(grid.index)
+    filter <- !is.na(grid.index)
     lat <- lat[filter]
     lon <- lon[filter]
     below.i <- sum(map.er.vector[er.i]>exc.rate.vector)
@@ -102,8 +106,8 @@ for(haz.i in 1:length(hazard.id)){
     map(database = "world", 
         xlim = c(min(lon), max(lon) + 20), 
         ylim = c(min(lat), max(lat)))
-    title(main = paste("VRISC ", hazard.name[haz.i], " at return period ", map.rp.vec[er.i],sep =""))
-    filter <- sample(x = length(lat), size = length(lon), replace = FALSE)
+    title(main = paste("VRISC ", hazard.meta$haz.name[haz.i], " at return period ", map.rp.vec[er.i],sep =""))
+    filter <- sample(x = length(lat), size = length(lat), replace = FALSE)
     #  filter <- sample(x = length(lat), size = 10000, replace = FALSE)
     points(lon[filter], lat[filter]
            , col=color.id[filter]
@@ -116,6 +120,8 @@ for(haz.i in 1:length(hazard.id)){
     )
     map(database = "world", add=TRUE) 
     dev.off()
+    s3.put(file = filename,
+             s3.path = paste(s3.output.path, filename, sep=""))
   }
 }
 

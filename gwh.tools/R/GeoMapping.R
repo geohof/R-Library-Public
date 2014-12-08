e3f11ea8338baff8d418af3c007b47836523bbf5
Georg Hofmann <georg.w.hofmann@gmail.com>
2014-12-08 09:02:46 -0500

    Added additional parameters

--- gwh.tools/R/GeoMapping.R
+++ gwh.tools/R/GeoMapping.R
@@ -29,15 +29,18 @@
 
 #' @export 
 #' @rdname geoMapping
-MapDots <- 
-  function(lon, lat, val,
-           val.bucket=seq(from = min(val), to = max(val), length.out = 6),
-           col.ramp.fun = colorRampPalette(c("gray", "yellow", "red")),
-           col = col.ramp.fun(length(val.bucket) - 1L),
-           leg.dec = as.integer(2 - log(min(val), base=10)),
-           leg.bucket = round(val.bucket, leg.dec),
-           leg.txt = paste(leg.bucket[1:(length(val.bucket) - 1L)], "-", leg.bucket[2:length(val.bucket)]),
-           file.name, title.str, leg.x="bottomright", change.order=TRUE
+MapDots <- function(
+  lon, lat, val,
+  val.bucket=seq(from = min(val), to = max(val), length.out = 6),
+  col.ramp.fun = colorRampPalette(c("gray", "yellow", "red")),
+  col = col.ramp.fun(length(val.bucket) - 1L),
+  leg.dec = as.integer(2 - log(min(val), base=10)),
+  leg.bucket = round(val.bucket, leg.dec),
+  leg.txt = paste(leg.bucket[1:(length(val.bucket) - 1L)], 
+                  "-", leg.bucket[2:length(val.bucket)]),
+  file.name, title.str, leg.x="bottomright", 
+  map.database="world", map.regions, map.lwd = 3,
+  change.order=TRUE
   ){
     if(change.order){
       o <- order(abs(val - median(val)))
@@ -48,19 +51,22 @@ MapDots <-
     if(!missing("file.name")){
       png(file.name, width = 2400, height = 1600, pointsize=48)
     }
-    
+
     num.bucket <- length(val.bucket) - 1L
-    map(database = "world", 
-        xlim = c(min(lon), max(lon)), 
-        ylim = c(min(lat), max(lat)))
+    if(missing(map.regions)){
+      map(database = map.database, 
+          xlim = c(min(lon), max(lon)), 
+          ylim = c(min(lat), max(lat)))
+    }else{
+      map(database = map.database, regions = map.regions)
+    }      
     
     val.col <- col[pmax(1L, pmin(findInterval(val, val.bucket), num.bucket))]
     points(lon, lat
            , col=val.col
            , pch=19
            , cex=.5)
-    map(database = "world", add=TRUE) 
-    map(database = "state", add=TRUE) 
+    map(database = map.database, add=TRUE, lwd = map.lwd) 
     if(!missing("title.str")){
       title(main = title.str)
     }
